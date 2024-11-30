68782300f6c7dc8073050eb01938afbba6712f18
unknown <jhonatalopes6042@digitalcollege.internal>
2024-11-30 17:01:55 -0300

    atualizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import image from "./assets/fitness-tracker-animate.svg"
 import { FaWeightHanging } from "react-icons/fa6"
+import { FaRuler } from "react-icons/fa"
+import { useState } from "react"
 
 function App() {
-  
+  const[peso, setPeso] = useState(0)
+  const[altura, setAltura] = useState(0)
+
+  function clickCalcular(){
+    const valorImc = peso / (altura * altura)
+    console.log(valorImc)
+  }
 
   return (
     <div className="w-full h-screen flex bg-black">
@@ -10,7 +18,7 @@ function App() {
         <img src={image} alt="" width={600}/>
       </div>
       <div className="w-[50%] h-full flex items-center justify-center">
-        <div className="w-[60%] h-auto min-h-[60%] p-[20px] rounded-lg flex flex-col bg-[#1f1f1f]">
+        <div className="w-[60%] h-auto p-[20px] rounded-lg flex flex-col bg-[#1f1f1f]">
           <div className="w-full flex flex-col ">
             <h1 className="text-white text-[35px] font-bold">Calculadora - IMC</h1>
           </div>
@@ -23,16 +31,40 @@ function App() {
                   <FaWeightHanging size={24} color="#b0b0b0ff"/>
                 </div>
                 <div className="w-[80%] h-full">
-                  <input type="text" />
+                  <input className="w-full h-full bg-transparent border-none outline-none text-white"  type="number" 
+                  onChange={
+                    (event) => setPeso(event.target.value)
+                  }
+                  />
                 </div>
                 <div className="w-[10%] h-full flex items-center justify-center">
-                  <p>Kg</p>
+                  <p className="text-white">Kg</p>
+                </div>
+              </div>
+            </div>
+            <div className="mt-4">
+              <label htmlFor="" className="text-white text-[18px]">Altura</label>
+              <div className="w-full flex bg-[#555555] rounded-md h-[40px]">
+                <div className="w-[10%] h-full flex items-center justify-center">
+                  <FaRuler size={24} color="#b0b0b0ff"/>
+                </div>
+                <div className="w-[80%] h-full">
+                  <input className="w-full h-full bg-transparent border-none outline-none text-white"  type="number"
+                  onChange={
+                    (event) => setAltura(event.target.value)
+                  }
+                  />
+                </div>
+                <div className="w-[10%] h-full flex items-center justify-center">
+                  <p className="text-white">m</p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="fim">
-
+          <div className="w-full flex flex-col items-center justify-center">
+            <button className="w-full h-[40px] bg-[#20a54c] rounded-lg text-white mt-4"
+            onClick={clickCalcular}
+            >Calcular</button>
           </div>
         </div>
       </div>
